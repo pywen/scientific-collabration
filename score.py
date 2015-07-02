f8a689602f5828364fa24a3444ddd3a92b42e437
wen <wenqidlut@hotmail.com>
2015-07-02 11:05:32 +0800

    Delete score.py

--- score.py
+++
@@ -1,9 +0,0 @@
-def score(scenes):
-'''count the times every agent appears in scenes(scenes have a attribute where a kind of agent appear,in this code it is
-   the index 1'''
-	agents=[]
-	for i in scenes:
-		agents.append(i[1])
-	sagents=list(set(agents))
-	count=[(cits.count(i),i) for i in agents]
-	return count
