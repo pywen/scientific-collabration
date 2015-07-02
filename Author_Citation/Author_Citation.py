13d2073fdfbdf1c5ab13b7c0ed2b0c002808b6dc
wen <wenqidlut@hotmail.com>
2015-07-02 15:34:19 +0800

    Update Author_Citation.py

--- Author_Citation/Author_Citation.py
+++ Author_Citation/Author_Citation.py
@@ -43,48 +43,48 @@ def a2b(lin):
     for i in dic:
         if i['ID']==int(lin[0]):
             au1=i['AF']
-	for i in dic:
-		if i['ID']==int(lin[1]):
-			au2=i['AF']
-	a2bedge=[]
-	for i in au1:
-		for j in au2:
-			a2bedge.append((auid[i],auid[j]))
-	return a2bedge
+    for i in dic:
+        if i['ID']==int(lin[1]):
+            au2=i['AF']
+    a2bedge=[]
+    for i in au1:
+        for j in au2:
+            a2bedge.append((auid[i],auid[j]))
+    return a2bedge
 	
 def au_cit_map(node,edge,nodefile,edgefile):
-  """get the csv format file of nodes and edges"""
-	fnode=open(nodefile+'.csv','w')
-	fnode.write('Id;Label\n')
-	for i in node:
-		fnode.write(str(i[0])+';'+i[1]+'\n')
-	fnode.close()
-	fedge=open(edgefile+'.csv','w')
-	fedge.write('Source;Target;Type;Weight\n')
-	for i in edge:
-		fedge.write(str(i[0])+';'+str(i[1])+';'+'Directed'+';'+str(i[2])+'\n')
-	fedge.close()
+    """get the csv format file of nodes and edges"""
+    fnode=open(nodefile+'.csv','w')
+    fnode.write('Id;Label\n')
+    for i in node:
+        fnode.write(str(i[0])+';'+i[1]+'\n')
+    fnode.close()
+    fedge=open(edgefile+'.csv','w')
+    fedge.write('Source;Target;Type;Weight\n')
+    for i in edge:
+        fedge.write(str(i[0])+';'+str(i[1])+';'+'Directed'+';'+str(i[2])+'\n')
+    fedge.close()
 
 def evo_au_citmap(year):
-  """get the author citation map file until certain year"""
-	papers=[]
-	lnode=[]
-	ledge=[]
-	edge=[]
-	for i in link:
-		for j in dic:
-			if str(j['ID'])==i[0] and int(j['PY'])<=year:
-				ledge.append(i)		
-	for i in ledge:
-		edge.extend(a2b(i))
-	edge=edgeweight(edge)
-	for i in ledge:
-		papers.extend(i)
-	papers=list(set(papers))
-	for i in papers:
-		for j in dic:
-			if j['ID']==int(i):
-				lnode.extend(j['AF'])
-	lnode=list(set(lnode))
-	node=[(auid[i],i) for i in lnode]				
-	au_cit_map(node,edge,str(year)+' cunode',str(year)+' cuedge')
+	"""get the author citation map file until certain year"""
+    papers=[]
+    lnode=[]
+    ledge=[]
+    edge=[]
+    for i in link:
+        for j in dic:
+            if str(j['ID'])==i[0] and int(j['PY'])<=year:
+                ledge.append(i)		
+    for i in ledge:
+        edge.extend(a2b(i))
+    edge=edgeweight(edge)
+    for i in ledge:
+        papers.extend(i)
+    papers=list(set(papers))
+    for i in papers:
+        for j in dic:
+            if j['ID']==int(i):
+                lnode.extend(j['AF'])
+    lnode=list(set(lnode))
+    node=[(auid[i],i) for i in lnode]				
+    au_cit_map(node,edge,str(year)+' cunode',str(year)+' cuedge')
